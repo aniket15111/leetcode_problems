29c38a1451a9d17f57cdb4ea7c5dce9d8b362fd0
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-02-26 01:05:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.py
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.py
@@ -1,27 +1,14 @@
 class Solution:
     def productExceptSelf(self, arr):
-        total_product = 1
-        zero_count = 0
-        
-        # First pass
-        for num in arr:
-            if num == 0:
-                zero_count += 1
-            else:
-                total_product *= num
-        
-        result = []
-        
-        # Second pass
-        for num in arr:
-            if zero_count > 1:
-                result.append(0)
-            elif zero_count == 1:
-                if num == 0:
-                    result.append(total_product)
-                else:
-                    result.append(0)
-            else:
-                result.append(total_product // num)
-        
-        return result
+        n = len(arr)
+        ans = [1] * n
+
+        for i in range(1, n):
+            ans[i] = ans[i-1] * arr[i-1]
+
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            ans[i] *= suffix
+            suffix *= arr[i]
+
+        return ans
