7ca5068b442a59aa2cc7b307691abfb2d85d97d0
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-03-16 19:31:13 +0530

    Time: 7 ms (57.61%) | Memory: 21.1 MB (96.57%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums1 = {}
+        cnt=0
+        element=0
 
         for i in nums:
-            if i not in nums1:
-                nums1[i] = 1
+            if cnt==0:
+                element = i
+                cnt+=1
+            elif i==element:
+                cnt+=1
             else:
-                nums1[i] += 1
-
-        return max(nums1, key=nums1.get)
+                cnt-=1
+        return element
+    
