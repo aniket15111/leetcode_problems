dd1fd3e9038e8ac801cdc6574b7484e1f8bbfcc0
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-03-21 17:15:40 +0530

    Time: 7 ms (28.25%) | Memory: 20.2 MB (51.29%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -2,16 +2,28 @@ class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         n=len(matrix)
         m=len(matrix[0])
-        row_mark=[0]*n
-        col_mark=[0]*m
-
+        # row_mark=[0]*n      matrix[row][0]
+        # col_mark=[0]*m      matrix[0][col]
+        col0=1
         for row in range(n):
             for col in range(m):
                 if matrix[row][col]==0:
-                    row_mark[row]=1
-                    col_mark[col]=1
+                    matrix[row][0]=0
+                    if col!=0:
+                        matrix[0][col]=0
+                    else:
+                        col0=0
 
-        for row in range(n):
-            for col in range(m):
-                if row_mark[row]==1 or col_mark[col]==1:
+        for row in range(1,n):
+            for col in range(1,m):
+                if matrix[row][0]==0 or matrix[0][col]==0: 
                     matrix[row][col]=0
+        
+        if matrix[0][0]==0:
+            for i in range(m):
+                matrix[0][i]=0
+        if col0==0:
+            for i in range(n):
+                matrix[i][0]=0
+
+        
