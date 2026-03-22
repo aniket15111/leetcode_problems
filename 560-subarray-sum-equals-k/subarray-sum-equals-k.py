421300ec9e4128005e51a5908ca8e24df5eec169
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-03-23 00:37:28 +0530

    Time: 29 ms (84.00%) | Memory: 21.8 MB (76.04%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix_sum=0
+        count=0
+        hash_map={0:1}
+        for i in nums:
+            prefix_sum+=i
+            if (prefix_sum-k) in hash_map:
+                count+=hash_map[(prefix_sum-k)]
+            if prefix_sum not in hash_map:
+                hash_map[prefix_sum]=1
+            else:
+                hash_map[prefix_sum]+=1
+        return count
