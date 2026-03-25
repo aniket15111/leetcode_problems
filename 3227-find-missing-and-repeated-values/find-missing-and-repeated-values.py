574d175aea008948bb841042b94edbc9852f7645
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-03-25 16:17:45 +0530

    Time: 7 ms (64.12%) | Memory: 19.6 MB (60.13%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -4,19 +4,21 @@ class Solution:
         total_elements = n * n
         num = set()
         repeat = 0
-        
+        expected_sum=0
+        count=1
+        actual_sum=0
         for i in range(n):
             for j in range(n):
                 if grid[i][j] in num:
                     repeat = grid[i][j]
                 else:
                     num.add(grid[i][j])
+                expected_sum+=count
+                actual_sum+=grid[i][j]
+                count+=1
         
 
-        missing = 0
-        for i in range(1, total_elements + 1):
-            if i not in num:
-                missing = i
-                break
+        missing = repeat-actual_sum+expected_sum
+        
                 
         return [repeat, missing]
