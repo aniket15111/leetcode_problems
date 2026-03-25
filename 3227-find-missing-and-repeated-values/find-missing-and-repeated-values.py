5cbf852e16a4eb241b4b64207ec20e0bfd0c73e5
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-03-25 16:18:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (29.89%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -1,24 +1,20 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         n = len(grid)
-        total_elements = n * n
+        m = n * n
         num = set()
         repeat = 0
-        expected_sum=0
-        count=1
-        actual_sum=0
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] in num:
-                    repeat = grid[i][j]
+        actual_sum = 0
+        
+        for row in grid:
+            for val in row:
+                actual_sum += val
+                if val in num:
+                    repeat = val
                 else:
-                    num.add(grid[i][j])
-                expected_sum+=count
-                actual_sum+=grid[i][j]
-                count+=1
+                    num.add(val)
         
-
-        missing = repeat-actual_sum+expected_sum
+        expected_sum = (m * (m + 1)) // 2
+        missing = repeat + expected_sum - actual_sum
         
-                
         return [repeat, missing]
