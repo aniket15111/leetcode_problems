c6572d4f1c84ff0c7b3531f2f94b4a6bda5f7b63
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-04-18 01:11:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.py
@@ -0,0 +1,39 @@
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+	
+class Solution:
+    def segregate(self, head):
+        if not head or not head.next:
+            return head
+
+        zeroD = Node(-1)
+        oneD = Node(-1)
+        twoD = Node(-1)
+
+        zero = zeroD
+        one = oneD
+        two = twoD
+
+        curr = head
+
+        while curr:
+            if curr.data == 0:
+                zero.next = curr
+                zero = zero.next
+            elif curr.data == 1:
+                one.next = curr
+                one = one.next
+            else:
+                two.next = curr
+                two = two.next
+            curr = curr.next
+
+        zero.next = oneD.next if oneD.next else twoD.next
+        one.next = twoD.next
+        two.next = None
+
+        return zeroD.next
