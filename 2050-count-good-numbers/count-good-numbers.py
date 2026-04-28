3a4db0f76ee7ac3399f39f492eb4784ee300c8e1
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-04-28 16:57:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.59%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -3,5 +3,19 @@ class Solution:
         even_ans=(n+1)//2
         odd_ans=n//2
         MOD = 10**9 + 7
-        ans=pow(5,even_ans,MOD)*pow(4,odd_ans,MOD)
+        ans=self.power(5,even_ans,MOD)*pow(4,odd_ans,MOD)
         return ans%MOD
+
+    def power(self,x,n,mod):
+        if n==0:
+            return 1
+        ans=1
+        x%=mod
+        while n>0:
+            if n%2==1:
+                ans=(ans*x)%mod
+
+            x=(x*x)%mod
+            n=n//2
+        return ans
+    
