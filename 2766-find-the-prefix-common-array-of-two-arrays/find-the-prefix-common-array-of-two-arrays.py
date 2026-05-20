618cf8cf09f3368406020e6abedfa03de4fab1b8
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-05-20 19:13:51 +0530

    Time: 11 ms (45.74%) | Memory: 19.2 MB (73.05%) - LeetSync

---
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        set1=set()
+        set2=set()
+        cmncnt=0
+        ans=[]
+        for i in range(len(A)):
+            set1.add(A[i])
+            set2.add(B[i])
+
+            if A[i]==B[i] :
+                cmncnt+=1
+            else:
+                if A[i] in set2:
+                    cmncnt+=1
+                if B[i] in set1:
+                    cmncnt+=1
+
+            set1.add(A[i])
+            set2.add(B[i])
+            ans.append(cmncnt)
+
+        return ans
+            
