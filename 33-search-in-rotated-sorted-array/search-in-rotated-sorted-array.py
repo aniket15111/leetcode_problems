f8242243ba7011877abef99488825c687628f2c5
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-05-22 15:58:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (10.24%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -3,23 +3,21 @@ class Solution:
         i=0
         j=len(arr)-1
 
-        while(i<=j):
+        while i<=j:
             mid=(i+j)//2
             if arr[mid]==target:
                 return mid
-
-            #identify the soreted half
             if arr[i]<=arr[mid]:
                 if arr[i]<=target<arr[mid]:
                     j=mid-1
-                else:
-                    i=mid+1
-            
-
+                else:   
+                    i=mid  +1
             else:
                 if arr[mid]<target<=arr[j]:
                     i=mid+1
-                else: j=mid-1
+                else:
+                    j=mid-1
+
 
         return -1
 
