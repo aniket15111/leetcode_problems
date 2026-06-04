63148df4ed82ad30d9ad154f2d7a264117b85279
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-06-04 22:08:04 +0530

    Time: 343 ms (52.49%) | Memory: 19.3 MB (69.73%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
@@ -2,27 +2,15 @@ class Solution:
     def totalWaviness(self, num1: int, num2: int) -> int:
 
         def peak_check(num):
-            prev = None
-            cnt = 0
-            while num > 0:
-                # Stop only when we don't even have a 'left' digit left to check
-                if num // 10 == 0:
-                    break
-                
-                if prev is None:
-                    prev = num % 10
-                
-                center = num % 10
-                left = (num // 10) % 10      
-                
-                if center > prev and center > left:
-                    cnt += 1
-                elif center < prev and center < left:
-                    cnt += 1
-                    
-                prev = center
-                num = num // 10
-                    
+            cnt=0
+            for i in range(1,len(num)-1):
+                centre=int(num[i])
+                left=int(num[i-1])  
+                right=int(num[i+1])
+                if centre>left and centre >right:
+                    cnt+=1
+                elif centre<left and centre<right:
+                    cnt+=1
             return cnt
 
         if num1 < 100:
@@ -33,5 +21,5 @@ class Solution:
 
         ans = 0
         for i in range(num1, num2 + 1):
-            ans += peak_check(i)
+            ans += peak_check(str(i))
         return ans
