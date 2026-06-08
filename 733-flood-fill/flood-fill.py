43b5ba295b065de5cc3020d73f681a79a1522ad4
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-06-09 00:54:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (16.52%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,29 +1,19 @@
 class Solution:
-    def dfs(self, image, i, j, new_color, original_color):
-        # 1. Check boundaries
-        # 2. Check if the current pixel is actually the color we want to change
-        if (i < 0 or i >= len(image) or 
-            j < 0 or j >= len(image[0]) or 
-            image[i][j] != original_color):
-            return
-
-        # Change the color of the current pixel
-        image[i][j] = new_color
-
-        # Recursively visit all 4 neighbors
-        self.dfs(image, i + 1, j, new_color, original_color) # Down
-        self.dfs(image, i - 1, j, new_color, original_color) # Up
-        self.dfs(image, i, j + 1, new_color, original_color) # Right
-        self.dfs(image, i, j - 1, new_color, original_color) # Left
-
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        original_color = image[sr][sc]
-        
-        # Optimization: If the target color is the same as the original, 
-        # doing a DFS will cause an infinite loop or redundant work.
-        if original_color == color:
+        rows,cols=len(image),len(image[0])
+        visited=set()
+        start_color=image[sr][sc]
+        if start_color == color:
             return image
-        
-        self.dfs(image, sr, sc, color, original_color)
-        
+        def dfs(r,c):
+            if (r<0 or r>=rows or c<0 or c>=cols or (r,c) in visited or image[r][c]!=start_color):
+                return 
+            
+            visited.add((r,c))
+
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            image[r][c]=color
+            for dr,dc in directions:
+                dfs(r+dr,c+dc)
+        dfs(sr,sc)
         return image
