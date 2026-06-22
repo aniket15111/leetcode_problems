00d7caec008c7903a61f09e14157e0a8b4c50b21
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-06-22 19:49:03 +0530

    Time: 3 ms (61.87%) | Memory: 19.3 MB (75.19%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,12 +1,20 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        st={'b':0,'a':0,'l':0,'o':0,'n':0}
+        b = a = l = o = n = 0
 
+        for ch in text:
+            if ch == 'b':
+                b += 1
+            elif ch == 'a':
+                a += 1
+            elif ch == 'l':
+                l += 1
+            elif ch == 'o':
+                o += 1
+            elif ch == 'n':
+                n += 1
 
-        for i in text:
-            if i in st:
-                st[i]+=1
+        return min(b, a, l // 2, o // 2, n)
 
-        return min(st['b'],st['a'],st['l']//2,st['o']//2,st['n'])
 
         
