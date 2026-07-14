77836d14f4ea6168caa9fff6fcedef4a373cda17
Aniket Kaushal <127053681+aniket15111@users.noreply.github.com>
2026-07-14 16:51:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (88.24%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.py
@@ -0,0 +1,31 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        lowest = 0
+        low_digit = len(str(abs(low)))
+        lowest_cnt = 1
+        lowest_add = 0
+        
+        for i in range(low_digit):
+            lowest = (lowest * 10) + lowest_cnt
+            lowest_cnt += 1
+            lowest_add = (lowest_add * 10) + 1
+            
+        lowest_ans = lowest
+        ans = []
+        
+        while lowest_ans <= high:
+            if lowest_ans >= low:
+                ans.append(lowest_ans)
+                
+            lowest_ans += lowest_add
+
+            if lowest_ans % 10 == 0 or lowest_ans % 10 < lowest_add % 10 or str(lowest_ans)[0] > str(10 - len(str(lowest_add))):
+                if len(str(lowest_add)) == 9:
+                    break
+                    
+                lowest_add = (lowest_add * 10) + 1
+                lowest = (lowest * 10) + lowest_cnt
+                lowest_cnt += 1
+                lowest_ans = lowest
+                
+        return ans
